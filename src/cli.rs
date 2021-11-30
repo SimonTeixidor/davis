66db6821186702aaff0d77e5e77cce5adc7b867d
Simon Persson <simon@flaskpost.me>
2021-11-30 19:08:44 +0100

    Remove old doc comments.

--- src/cli.rs
+++ src/cli.rs
@@ -202,93 +202,59 @@ fn lexopt_parse_args() -> Result<Opts, lexopt::Error> {
 }
 
 pub struct Opts {
-    /// The MPD server, can be specified using IP/hostname, or a label defined in the config file.
     pub host: Option<String>,
-    /// Enable verbose output.
     pub verbose: bool,
     pub plain_formatting: bool,
     pub subcommand: Option<SubCommand>,
 }
 
 pub enum SubCommand {
-    /// Display the currently playing song.
     Current,
-    /// Start playback.
     Play {
-        /// Queue position to start playing from, defaults to 1.
         position: Option<NonZeroU32>,
     },
-    /// Pause playback.
     Pause,
-    /// Toggle between play/pause.
     Toggle,
-    /// List items in path.
     Ls {
         path: Option<String>,
     },
-    /// Clear the current queue.
     Clear,
-    /// Skip to next song in queue.
     Next,
-    /// Go back to previous song in queue.
     Prev,
-    /// Stop playback.
     Stop,
-    /// Add items in path to queue.
     Add {
         path: String,
     },
-    /// Load playlist at path to queue.
     Load {
         path: String,
     },
-    /// Display the current queue.
     Queue,
-    /// Search the MPD database.
     Search {
         query: SearchQuery,
     },
-    /// List all values for tag type, matching query.
     List {
-        /// List values for this tag type
         tag: String,
         query: SearchQuery,
     },
-    /// Read raw metadata tags for file.
     ReadComments {
         file: String,
     },
-    /// Update the MPD database.
     Update,
-    /// Display MPD status.
     Status,
-    /// Download albumart for track.
     Albumart {
-        /// The song to fetch albumart for. Will use currently playing song if not provided.
         song_path: Option<String>,
-        /// Path to a file where the image will be stored. The image can be written to stdout by
-        /// supplying a '-' character.
         output: String,
     },
-    /// Move song in queue.
     Mv {
-        /// Queue index of song to move
         from: NonZeroU32,
-        /// Position in queue to move song to
         to: NonZeroUsize,
     },
-    /// Remove song from queue.
     Del {
-        /// Queue index
         index: NonZeroU32,
     },
-    /// Set current playback time.
     Seek {
-        /// Position to seek to, expressed in [+-][[hh:]:mm]:ss format. If + or - is used, the seek
-        /// is done relative to the current positon.
         position: seek::SeekArg,
     },
-    /// List all files prefixed by path, used for tab completion.
     Tab {
         path: String,
     },
@@ -296,7 +262,6 @@ pub enum SubCommand {
 }
 
 pub struct SearchQuery {
-    /// Either a single value with a search expression, or a sequence of key-value pairs.
     pub query: Vec<String>,
 }
 
